5eb36b91f5346487a68fa1af2d17d314eb091eb7
Renan Tinti <renantinti@gmail.com>
2022-10-09 11:45:04 -0300

    files structure finished

--- index/index.js
+++ index/index.js
@@ -1,7 +1,30 @@
 $(document).ready(function(){
     console.log('jquery working');
     $('#timer-content').click(function(){
-        console.log('button clicked');
         $('#content').load('../timer/timer.html');
+    })
+
+    $('#calculator-content').click(function(){
+        $('#content').load('../calculator/calculator.html');
+    })
+
+    $('#drums-content').click(function(){
+        $('#content').load('../drums/drums.html');
+    })
+
+    $('#colorsgame-content').click(function(){
+        $('#content').load('../colors_game/colors_game.html');
+    })
+
+    $('#tictactoe-content').click(function(){
+        $('#content').load('../tic_tac_toe/tic_tac_toe.html');
+    })
+
+    $('#tipcalculator-content').click(function(){
+        $('#content').load('../tip_calculator/tip_calculator.html');
+    })
+
+    $('#todo-content').click(function(){
+        $('#content').load('../todo_list/todo_list.html');
     })
 })
