93ad8328e4930260de1bf87fe13101361451cb26
Renan Tinti <renantinti@gmail.com>
2022-10-17 10:12:43 -0300

    timer buttons working

---
+++ timer/timer.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("#btn-start").click(function(){
+        console.log("start clicked");
+    })
+
+    $("#btn-stop").click(function(){
+        console.log("start clicked");
+    })
+
+    $("#btn-resume").click(function(){
+        console.log("start clicked");
+    })
+})
